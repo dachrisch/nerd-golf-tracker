08e4334ff18d66700f38c608eb8bc57e826c31a8
Christian Dähn <christian.daehn@it-agile.de>
2011-12-05 16:07:26 +0100

    unbekannte operation wird erkannt und abgefangen

--- unittests/de/itagile/golf/EinfacherIntepreterTest.java
+++ unittests/de/itagile/golf/EinfacherIntepreterTest.java
@@ -14,6 +14,7 @@ import org.junit.runners.Parameterized.Parameters;
 import de.itagile.golf.operation.Hilfe;
 import de.itagile.golf.operation.Lochwechsel;
 import de.itagile.golf.operation.Schlag;
+import de.itagile.golf.operation.UnbekannteOperation;
 
 @RunWith(Parameterized.class)
 public class EinfacherIntepreterTest {
@@ -33,6 +34,7 @@ public class EinfacherIntepreterTest {
 				{"Schlage Ball", Schlag.class},
 				{"Nächstes Loch", Lochwechsel.class},
 				{"Hilfe", Hilfe.class},
+				{"blablub", UnbekannteOperation.class},
 				};
 		return Arrays.asList(testData);
 	}
